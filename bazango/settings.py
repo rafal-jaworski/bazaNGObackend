08a43e940223b9eda76bbf53b9a263ebf5e2ec15
Rafał Jaworski <rafal.jaworski@bravelab.io>
2017-09-09 08:44:39 +0200

    Heroku conf 4

--- bazango/settings.py
+++ bazango/settings.py
@@ -129,14 +129,17 @@ LANGUAGES = [
   ('en', _('English')),
 ]
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
