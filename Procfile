5e2174d7567a612bb9c9a198a821807231d278e9
Rafał Jaworski <rafal.jaworski@bravelab.io>
2017-09-25 21:09:44 +0200

    Add docker support

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn bazango.wsgi:application --log-file -
---
+++ src/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
